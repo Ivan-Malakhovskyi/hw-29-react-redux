4ec69169112598e3a6517f73704d56a3be76d870
Ivan-Malakhovskyi <124193519+Ivan-Malakhovskyi@users.noreply.github.com>
2026-08-02 11:06:59 +0300

    add changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from "react-router";
+import { useState } from "react";
 import { Layout } from "./components/Layout";
 import { PrivateRoute } from "./components/PrivateRoute";
 import { RestrictedRoute } from "./components/RestrictedRoute";
@@ -7,10 +8,43 @@ import HomePage from "./pages/Home";
 import RegisterPage from "./pages/Register";
 import LoginPage from "./pages/Login";
 import UsersPage from "./pages/Users";
+import styles from "./components/Modal/Modal.module.css";
+import { WithoutPortal } from "./components/Modal/WithoutPortal";
+import { WithPortal } from "./components/Modal/WithPortal";
 
 export const App = () => {
+  const [isOpenWith, setIsOpenWith] = useState(false);
+  const [isOpenWithout, setIsOpenWithout] = useState(false);
+
+  const handleToggleWithout = () => {
+    setIsOpenWithout(!isOpenWithout);
+  };
+
+  const handleToggleWith = () => {
+    setIsOpenWith(!isOpenWith);
+  };
+
   return (
     <>
+      <div className={styles["kill-container"]}>
+        Open Without
+        <button type="button" onClick={handleToggleWithout}>
+          Open
+        </button>
+        <WithoutPortal
+          handleToggle={handleToggleWithout}
+          isOpen={isOpenWithout}
+        />
+      </div>
+
+      <div className={styles["kill-container"]}>
+        <button type="button" onClick={handleToggleWith}>
+          Open
+        </button>
+
+        <WithPortal handleToggle={handleToggleWith} isOpen={isOpenWith} />
+      </div>
+
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />} />
